67e7f4c2084cc3e6f6a739c6a414ae12d8311341
mehrlean <66731443+mehrlean@users.noreply.github.com>
2023-04-04 16:29:26 +0200

    Implement event listeners for popup UI buttons.

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById('hideBtn').addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.tabs.sendMessage(tab.id, { action: 'hideShorts' });
+  });
+  
+  document.getElementById('showBtn').addEventListener('click', async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.tabs.sendMessage(tab.id, { action: 'showShorts' });
+  });
+  
