f4bf612790ff55e52dbc06a4e83685d9817ee5ac
mehrlean <66731443+mehrlean@users.noreply.github.com>
2023-04-04 16:29:58 +0200

    Functions to hide and restore elements containing YouTube shorts

---
+++ content.js
@@ -0,0 +1,31 @@
+let hiddenElements = [];
+
+function removeElementsWithShorts() {
+  console.log('Hiding elements with "/shorts/"');
+  const anchors = document.getElementsByTagName('a');
+  const anchorArray = Array.from(anchors);
+  anchorArray.forEach((anchor) => {
+    const href = anchor.getAttribute('href');
+    if (href && href.includes('/shorts/')) {
+      const grandparent = anchor.parentElement.parentElement.parentElement;
+      grandparent.style.display = 'none';
+      hiddenElements.push(grandparent);
+    }
+  });
+}
+
+function restoreHiddenElements() {
+  console.log('Showing elements with "/shorts/"');
+  hiddenElements.forEach((element) => {
+    element.style.display = '';
+  });
+  hiddenElements = [];
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'hideShorts') {
+    removeElementsWithShorts();
+  } else if (request.action === 'showShorts') {
+    restoreHiddenElements();
+  }
+});
