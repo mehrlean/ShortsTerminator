c12d23419b67da5e603cbc9c62fff23180633fbc
mehrlean <66731443+mehrlean@users.noreply.github.com>
2023-04-04 16:32:34 +0200

    Changed default state to hidden

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
+chrome.tabs.sendMessage(tab.id, { action: 'hideShorts' });
+
 document.getElementById('hideBtn').addEventListener('click', async () => {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.tabs.sendMessage(tab.id, { action: 'hideShorts' });
   });
   
   document.getElementById('showBtn').addEventListener('click', async () => {
